cd3dc097ec04e24a6194faab2aa2b56e0a0460a1
Burabyo <j.burabyo@alustudent.com>
2025-11-24 13:01:09 +0200

    Starting with Priority.java for better orginisation

---
+++ src/tms/model/Priority.java
@@ -0,0 +1,18 @@
+package tms.model;
+
+public enum Priority {
+    LOW,
+    MEDIUM,
+    HIGH;
+
+    public static Priority fromString(String s) {
+        if (s == null) return MEDIUM;
+        switch (s.trim().toUpperCase()) {
+
+            case "LOW": return LOW;
+            case "HIGH": return HIGH;
+            default: return MEDIUM;
+        }
+    }
+
+}
